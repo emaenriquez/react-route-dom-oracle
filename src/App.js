7c23f7615a33e178db8c5e535ac010aa1e7347ef
emaenriquez1985 <emaenriquez1985@gmail.com>
2024-06-18 00:03:43 +0000

    creando las primeras rutas con react-route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element = { <Home/> } ></Route>
+        <Route path='/sobre' element = { <Sobre/> }></Route>
+      </Routes>
+    </Router>
   );
 }
 
